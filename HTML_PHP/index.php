6beac51676a368ee24c286eeda2cfbe366324a48
githubpat1 <github.pat@gmail.com>
2020-02-20 15:06:27 +0100

    Rename

--- HTML_PHP/index.php
+++ HTML_PHP/index.php
@@ -5,28 +5,29 @@
   <head>
     <meta charset="utf-8">
     <title>BBS-Syke Elternsrpechtag</title>
-    <link rel="stylesheet" href="stylee.css" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link rel="stylesheet" href="stylee.css" type="text/css">
+    
   </head>
   <!-- Body------------------------------------------------------ -->
   <body>
     <div id="wrapper">
-  <?php
-    require("header.php");
-  ?>
+      <?php
+        require("header.php");
+      ?>
 <!-- Navigations Leiste----------------------------------------------- -->
-  <?php
-    //require("navigation.php");
-  ?>
+      <?php
+        //require("navigation.php");
+      ?>
 <!-- Main------------------------------------------------------ -->
     
-  <?php
-    //require("login.php");
-  ?>
+      <?php
+        //require("login.php");
+      ?>
 <!-- Footer------------------------------------------------------ -->
-  <?php
-    //require("footer.php");
-  ?>    
+      <?php
+        //require("footer.php");
+      ?>    
     </div>
   </body>
 </html>
