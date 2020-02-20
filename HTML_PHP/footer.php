99ec651546fece4f79fa6dfaca48eedac8771eb2
githubpat1 <61010668+githubpat1@users.noreply.github.com>
2020-02-20 14:24:03 +0000

    Delete footer.php

--- HTML_PHP/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-      <ul>
-        <li><a href="https://www.bbs-syke.de/portal/kontakt.html">Kontakt</li>
-        <li><a href="https://www.bbs-syke.de/portal/seiten/datenschutz-1002-2186.html">Datenschutz</li>
-        <li><a href="https://www.bbs-syke.de/portal/seiten/impressum-1001-2186.html">Impressum</li>
-      </ul>
-    </footer>
