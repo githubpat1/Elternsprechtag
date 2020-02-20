25825fd7d569eaf13829fc45df3c121b3c20c14b
githubpat1 <github.pat@gmail.com>
2020-02-20 15:53:22 +0100

    Neu Anfang

---
+++ HTML/main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BBS Syke</title>
+    <meta name="viewport" content="width=device-width, inital-scale=10.0"/>
+    <link rel="stylesheet" type="text/css" href ="/Projekte/Elternsprechtag/CSS/style.css">
+</head>
+
+<body>
+<div class="wrapper">
+    <div class="box">Die erste box</div>
+    <div class="box">Die zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite boxDie zweite box</div>
+    <div class="box">Die dritte box</div>
+    <div class="box">Die viere box</div>
+</div>
+</body>
+</html>
