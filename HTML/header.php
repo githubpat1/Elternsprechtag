481d77f027048b4b2f851ae1463daecd46f26273
Niko <github.thai@gmail.com>
2020-02-24 14:48:43 +0100

    Header inhalt positioniert und formatiert

--- HTML/header.php
+++ HTML/header.php
@@ -1,9 +1,11 @@
 <header>
-<div class="bild">
-    <div class="header_bild"><img src="../Bilder/bbs-syke_logo.png" alt="Logo BBS Syke"></div>
-</div>
-<div class="links">
-    <div class="header_links"><a href="https://www.bbs-syke.de/">Startseite</a></div>
-    <div class="header_links"><a href="https://www.bbs-syke.de/portal/kontakt.html">Kontakt</a></div>
-</div>
+  <div class="bild">
+      <div class="header_bild"><a href=""><img src="../Bilder/bbs-syke_logo.png" alt="Logo BBS Syke"></a></div>
+  </div>
+  <div class="links">
+      <div class="header_links"><a href="https://www.bbs-syke.de/">Startseite</a></div>
+      <div class="header_links"><a href="https://www.bbs-syke.de/portal/kontakt.html">Kontakt</a></div>
+      <div class="header_links"><a href="">Login</a></div>
+      <div class="header_links"><a href="">usw</a></div>
+  </div>
 </header>
