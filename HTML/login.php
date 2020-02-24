b5a07b432c80b140d88a75f5f03172567f6d535e
unknown <nikolas.seliger@bbs-syke.schule>
2020-02-24 09:07:47 +0100

    Login PHP code fix copy paste

--- HTML/login.php
+++ HTML/login.php
@@ -13,10 +13,32 @@
 
     <?php
 
-    //session_start();
+    session_start();
 
     /*if(isset($_SESSION['userid'])){
       header("Location: ../index.php");
     }*/
+
+
+    $password = $_POST["password"];
+
+    $pdo = new PDO('mysql:host=localhost;dbname=elternsprechtag' , 'root', '');
+
+    $statement = $pdo->prepare("SELECT * FROM bucherindex WHERE UUID = :username");
+    $statement->execute(array('username' => $username));
+    $benutzer = $statement->fetch();
+
+    print_r($benutzer);
+
+    if(!empty($password)){
+      if(!$benutzer || $password != $benutzer['Passwort']){
+        echo $_SESSION['msgError'] = "Benutzername oder Passwort ist falsch";
+        //header("Location: login.php");
+      }else{
+        echo  $_SESSION['userid'] = $benutzer['ID'];
+        echo  $_SESSION['username'] = $username;
+        //header("Location: index.php");
+      }
+    }
 
     ?>
