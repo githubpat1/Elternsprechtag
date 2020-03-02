b2e489bedcee5964b83c626843b4ee1f97785ffc
unknown <patrick.costasduran@bbs-syke.schule>
2020-03-02 08:29:52 +0100

    header: login link aktualisiert; login: username -> vorname / nachname

--- HTML/header.php
+++ HTML/header.php
@@ -5,7 +5,7 @@
   <div class="links">
       <div class="header_links"><a href="https://www.bbs-syke.de/">Startseite</a></div>
       <div class="header_links"><a href="https://www.bbs-syke.de/portal/kontakt.html">Kontakt</a></div>
-      <div class="header_links"><a href="">Login</a></div>
+      <div class="header_links"><a href="http://localhost/Elternsprechtag/HTML/login.php">Login</a></div>
       <div class="header_links"><a href="">usw</a></div>
   </div>
 </header>
