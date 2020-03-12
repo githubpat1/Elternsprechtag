245d940c858078a3b1a9ab43b5c021341f12bf04
patrick.costasduran@bbs-syke.schule <patrick.costasduran@bbs-syke.schule>
2020-03-12 12:42:56 +0100

    Mobile kram, button weg

--- HTML/header.php
+++ HTML/header.php
@@ -2,28 +2,13 @@
 session_start();
 ?>
 
-<script>
-function menuResponsive() {
-  var x = document.getElementById("test");
-  if (x.className === "links") {
-    x.className += " responsive";
-  } else {
-    x.className = "links";
-  }
-}
-</script>
-
 <header>
   <div class="bild">
       <div class="header_bild"><a href=""><img src="../Bilder/bbs-syke_logo.png" alt="Logo BBS Syke"></a></div>
   </div>
   <div class="links" id="test">
       <a class="header_links" id="q" href="https://www.bbs-syke.de/">Startseite</a>
-      <a class="header_links" id="q" href="https://www.bbs-syke.de/portal/kontakt.html">Kontakt</a>
-      <a class="header_links" id="q" href="http://localhost/Elternsprechtag/HTML/login.php">Login</a>
-
-      <a href="javascript:void(0);" class="icon" onclick="menuResponsive()">
-        <i class="fa fa-bars"><img src="../Bilder/menubutton.png" alt="Menu Icon"></i>
-      </a>
-    </div>
+      <a class="header_links" id="q linksNachUnten" href="https://www.bbs-syke.de/portal/kontakt.html">Kontakt</a>
+      <a class="header_links" id="q linksNachUnten" href="http://localhost/Elternsprechtag/HTML/login.php">Login</a>
+  </div>
 </header>
