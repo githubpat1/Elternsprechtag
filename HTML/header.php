87ff177e23616a88606782d1c3a43bcb39c79798
patrick.costasduran@bbs-syke.schule <patrick.costasduran@bbs-syke.schule>
2020-03-13 12:04:42 +0100

    Color Change (HexCode in RGB) Style der Nav in Responsive verbessert

--- HTML/header.php
+++ HTML/header.php
@@ -7,8 +7,8 @@ session_start();
       <div class="header_bild"><a href=""><img src="../Bilder/bbs-syke_logo.png" alt="Logo BBS Syke"></a></div>
   </div>
   <div class="links" id="test">
-      <a class="header_links" id="q" href="https://www.bbs-syke.de/">Startseite</a>
-      <a class="header_links" id="q" href="https://www.bbs-syke.de/portal/kontakt.html">Kontakt</a>
-      <a class="header_links" id="q" href="http://localhost/Elternsprechtag/HTML/login.php">Login</a>
+      <a class="header_links" id="firstLink" href="https://www.bbs-syke.de/">Startseite</a>
+      <a class="header_links" href="https://www.bbs-syke.de/portal/kontakt.html">Kontakt</a>
+      <a class="header_links" href="http://localhost/Elternsprechtag/HTML/login.php">Login</a>
   </div>
 </header>
